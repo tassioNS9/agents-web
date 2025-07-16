03c2b424ec7a9ceee9c3dcae58626929047baed6
Tassio Neves <tassions@hotmail.com>
2025-07-15 23:12:54 -0300

    feat: add create room and get list room

---
+++ src/lib/dayjs.ts
@@ -0,0 +1,7 @@
+import lib from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+lib.locale("pt-BR");
+lib.extend(relativeTime);
+
+export const dayjs = lib;
--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,14 +1,15 @@
-import { Room } from "./room";
-
+import { CreateRoomForm } from "@/components/create-room-form";
+import { RoomList } from "@/components/room-list";
 
 export function CreateRoom() {
   return (
     <div className="min-h-screen px-4 py-8">
       <div className="mx-auto max-w-4xl">
-        <div className="grid grid-cols-2 text-red-600 items-start gap-8">
-            <Room/>
+        <div className="grid grid-cols-2 items-start gap-8">
+          <CreateRoomForm />
+          <RoomList />
         </div>
       </div>
     </div>
-  )
+  );
 }
